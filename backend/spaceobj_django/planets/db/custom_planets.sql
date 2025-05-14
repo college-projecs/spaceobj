ff1fcf48cafb833fcb0bc8f0fd2c3fab6eab897f
Alpq <57579939+Alpq@users.noreply.github.com>
2025-05-14 10:30:53 -0400

    Update the custom_planets sql table so that it tak es in the correct values.

--- backend/spaceobj_django/planets/db/custom_planets.sql
+++ backend/spaceobj_django/planets/db/custom_planets.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS custom_planets (
-    planet_id SERIAL PRIMARY KEY,            
-    planet_name VARCHAR(255) NOT NULL,
-    mass INTEGER NOT NULL,
-    radius INTEGER NOT NULL,
-    climate_type INTEGER NOT NULL,
+    planet_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     seed FLOAT NOT NULL,
-    notes TEXT,
+    planet_size  FLOAT NOT NULL,
+    orbit_radius FLOAT NOT NULL,
+    axial_tilt FLOAT NOT NULL,
+    orbit_speed FLOAT NOT NULL,
+    water_threshold FLOAT NOT NULL,
+    show_rings BOOLEAN NOT NULL,
+    color_mode VARCHAR(50) NOT NULL,
+    gas_type   VARCHAR(50) NOT NULL
 );
