5eb7bf6662c0c3ce564c4c0229606a36f9c52419
Alpq <57579939+Alpq@users.noreply.github.com>
2025-05-23 22:32:33 -0400

    Add some comments to our largest page Custom Custo mPlanets so the professor can understand what's go ing on.

--- front-end/src/pages/CustomPlanets.tsx
+++ front-end/src/pages/CustomPlanets.tsx
@@ -15,6 +15,12 @@ import { Header } from './Homepage';
 import { Hero } from './Homepage';
 import { Footer } from './Homepage';
 
+/* AnimatedOrbitProps and AnimatedOrbit are tools from the planets component
+that keep updating the visuals and allow for the radius and speed to update 
+as the sliders change them around. We had to change rotation to y axis here
+since x axis rotation in planets is parallel with the x axis and that means
+they are indistinguishable from regular rotation.
+*/
 type AnimatedOrbitProps = {
   radius: number;
   speed: number;
@@ -37,9 +43,44 @@ function AnimatedOrbit({
     </group>
   );
 }
+/* CustomPlanets is the main visual element of this page (we could have made
+this a component in hindsight). 
 
+First, we define the ranges of the sliders (realisticValues)
+that work well with the display page on the site
+
+Next, we define the parameters that define a custom planet as a list of constants.
+The useStates define some default value that is uploaded when you first open the page.
+We generate a new seed parameter using Math.random().
+
+The createPlanetTexture function is what allows us to procedurally generate the planet
+textual. It takes in whether the planet is a gas or earth-type and also takes in
+the waterThreshold for earth-type planets. 
+
+  First a Uint8Array array stores a large set of values corresponding to each pixel
+  on the texture (hence, it is sized dimensionxdimension).
+
+  Next we iterate through this array in a for loop that uses mod(dimension) to
+  define rows and coluumns in the 1D array. 
+  
+    Each pixel is given assigned a value in cartesian coordinates using its position in spherical coordinates. 
+    
+      Spherical coordinates use theta to determine horizontal rotation and phi to define vertical
+      rotation, and, with some Calc III, can be used to convert that into an x,y,z value. If we
+      want to make streaks for a gaseous planet the y values are multiplied by 10
+      which is similar to a horizontal dilation that stretches patches of color. 
+    
+    Every single coordinate is given some value through the Noise() random number generator. 
+    A switch is used to differentiate between earth-type and gas-type color logic: assigns a
+    color to the Uint8Array pixel in that position. The colors were hard coded for the different
+    atmosphere types, and green and blue for land and water. For some reason the buffer array,
+    which stores all the (R,G,B values) inside the for loop, could not be used to wrap our texture.
+
+    Finally this U8intArray is wrapped around the planet object and its visuals are updated.
+
+
+*/
 export default function CustomPlanets() {
-  // realistic ranges
   const realisticValues = {
     orbitRadius: { min: 0.1, max: 5, scale: 5 },
     orbitSpeed: { min: 0.01, max: 5, scale: 0.5 },
@@ -131,6 +172,11 @@ export default function CustomPlanets() {
     setTexture(tex);
   }
 
+  /*
+  savePlanet is a post function that allows for the parameters of a custom planet to be 
+  sent to the backend through a JSON, similar to the example we worked in class (save message
+  was added too).
+  */ 
   function savePlanet() {
     fetch('http://127.0.0.1:8000/Create_planet/', {
       method: 'POST',
@@ -150,6 +196,9 @@ export default function CustomPlanets() {
     }).then(() => alert('planet saved!'));
   }
 
+  /*
+  This is the live component of the website that displays all of the components.
+  */ 
   return (
     <div>
       <Header />
@@ -158,9 +207,10 @@ export default function CustomPlanets() {
       <section className="intro custom-section">
         <div className="intro-container">
           <div className="custom-content">
+            {/*/ custom-controls holds the sidebar with all the sliders*/}
             <aside className="custom-controls">
               <label>
-                <span>Name</span>
+                <span>Name</span> // Updating the name of the planet.
                 <input
                   type="text"
                   className="custom-input"
@@ -172,7 +222,12 @@ export default function CustomPlanets() {
               </label>
 
               <label>
-                <span>Mode</span>
+                <span>Mode</span> 
+                {/* 
+                Switching between earth-style and gaseous planets. 
+                I was familiar with sliders at the time 
+                but this could have been a dropdown as well.
+                */}
                 <select
                   className="custom-select"
                   value={colorMode}
@@ -181,7 +236,8 @@ export default function CustomPlanets() {
                       e.target.value as any
                     )
                   }
-                >
+                
+                > 
                   <option value="terrain">
                     Earth Type
                   </option>
@@ -218,7 +274,13 @@ export default function CustomPlanets() {
                   />
                 </label>
               )}
-
+                {/* 
+                Setting the size of the planet 
+                The custom-range class is used throughout the numerical
+                parmaeters to display the realistic values of the planet 
+                parameters in astronomical units without
+                actually changing the visual size.
+                */}
               <label>
                 <span>
                   Size: {planetSize.toFixed(1)}×
@@ -235,7 +297,9 @@ export default function CustomPlanets() {
                   }
                 />
               </label>
-
+                {/* 
+                Setting the orbit size of the planet
+                */}
               <label>
                 <span>
                   Orbit Radius:{' '}
@@ -257,7 +321,10 @@ export default function CustomPlanets() {
                   }
                 />
               </label>
-
+                {/* 
+                Setting the axial tilt of the planet, which
+                is more apparent when rings are turned on.
+                */}
               <label>
                 <span>Axial Tilt: {axialTilt}°</span>
                 <input
@@ -272,7 +339,9 @@ export default function CustomPlanets() {
                   }
                 />
               </label>
-
+                {/* 
+                Setting the orbital speed of the planet.
+                */}
               <label>
                 <span>
                   Orbit Speed:{' '}
@@ -294,7 +363,11 @@ export default function CustomPlanets() {
                   }
                 />
               </label>
-
+                {/* 
+                If the user chooses terrain (earth-style graphics),
+                then they can set the water threshold which controls
+                the createPlanetTexture output.
+                */}
               {colorMode === 'terrain' && (
                 <label>
                   <span>
@@ -316,7 +389,9 @@ export default function CustomPlanets() {
                   />
                 </label>
               )}
-
+                {/* 
+                The user can choose to have rings on their planet.
+                */}
               <label>
                 <input
                   type="checkbox"
@@ -328,12 +403,19 @@ export default function CustomPlanets() {
                 />{' '}
                 Show Rings
               </label>
-
+                {/* 
+                Displays the seed. This was useful for testing
+                during save function since it gave a unique
+                corresponding value between our front-end and 
+                postgreSQL database
+                */}
               <div className="seed-display">
                 <strong>Seed:</strong>{' '}
                 {seed.toFixed(6)}
               </div>
-
+                {/* 
+                Resets the seed and reruns the texture generation.
+                */}
               <button
                 className="btn regenerate-btn"
                 onClick={() => {
@@ -343,7 +425,9 @@ export default function CustomPlanets() {
               >
                 Re-generate Texture
               </button>
-
+                {/* 
+                Runs the POST function for the backend.
+                */}
               <button
                 className="btn save-btn"
                 onClick={savePlanet}
@@ -351,7 +435,13 @@ export default function CustomPlanets() {
                 Save Planet
               </button>
             </aside>
+                {/* 
+                This configures the black background arround the planet
+                in the canvas container, along with the shading, orbit and radius.
 
+                The radius and speed defined by the user are scaled down
+                by the display values so they aren't too big.
+                */}
             <div className="canvas-container">
               <Canvas>
                 <ambientLight intensity={0.5} />
